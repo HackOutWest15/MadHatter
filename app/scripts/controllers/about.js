6f11319432be67a9388417eb4ff8aca464c63a7c
chrissew <christoffer.wallenberg@gmail.com>
2015-08-11 11:47:49 +0200

    Initial project structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name madHatterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the madHatterApp
+ */
+angular.module('madHatterApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name madHatterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the madHatterApp
+ */
+angular.module('madHatterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
